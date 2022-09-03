076596dbb33dbccd8ee426eff42570359b0bbe30
bagotaj <bagotaj@gmail.com>
2022-09-03 15:47:44 +0200

    Adding header part

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header>
+
+    <nav class="menu">
+      <ul>
+        <li>A KELET KÖNYVEI</li>
+        <li class="loggedinContainer displaynone">
+              <div id="navbarLi2">
+                <div class="form-check">
+                  <p class="content"></p>
+                  <button
+                    type="button"
+                    class="normalbtn"
+                    value="logout"
+                    id="logoutButton"
+                  >
+                    Kilépés
+                  </button>
+                </div>
+              </div>
+            </li>
+      </ul>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </nav>
+  </header>
   <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
