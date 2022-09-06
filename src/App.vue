9cc7390f0f82ddc7914682f75cd4bfcd6651be66
bagotaj <bagotaj@gmail.com>
2022-09-06 18:48:00 +0200

    Moving navbar part to a NavView component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 <template>
-  <header>
-
-    <nav class="menu">
-      <ul>
-        <li>A KELET KÖNYVEI</li>
-        <li class="loggedinContainer displaynone">
-              <div id="navbarLi2">
-                <div class="form-check">
-                  <p class="content"></p>
-                  <button
-                    type="button"
-                    class="normalbtn"
-                    value="logout"
-                    id="logoutButton"
-                  >
-                    Kilépés
-                  </button>
-                </div>
-              </div>
-            </li>
-      </ul>
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    </nav>
-  </header>
+  <NavView />
   <router-view />
 </template>
+
+<script>
+  import NavView from '@/views/NavView.vue'
+
+  export default {
+    components: { NavView },
+  }
+</script>
