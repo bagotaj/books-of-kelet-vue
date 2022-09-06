7bda83d08b28683d777a411759d6a763dafe9e70
bagotaj <bagotaj@gmail.com>
2022-09-06 18:48:52 +0200

    Creating a NavView navbar component

---
+++ src/views/NavView.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav class="menu">
+      <ul>
+        <li>A KELET KÖNYVEI</li>
+        <li class="loggedinContainer" v-if="store.display">
+              <div id="navbarLi2">
+                <div class="form-check">
+                  <p class="content"></p>
+                  <button
+                    type="button"
+                    class="normalbtn"
+                    value="logout"
+                    id="logoutButton"
+                  >
+                    Kilépés
+                  </button>
+                </div>
+              </div>
+            </li>
+        </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+    import { store } from '../composables/store.js';
+
+    export default {
+        data() {
+            return {
+                store
+            }
+        }
+    }
+</script>
