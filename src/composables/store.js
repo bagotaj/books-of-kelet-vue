d179b717d9aafdb687b0083f02c575512fab4265
bagotaj <bagotaj@gmail.com>
2022-09-13 18:13:09 +0200

    Adding new states

--- src/composables/store.js
+++ src/composables/store.js
@@ -2,6 +2,24 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   display: false,
+  savedBasicsShelvesData: [],
+  dataOfImageToFirebase: {},
+  resizedLocalImageFile: null,
+  setClickCanvas: null,
+  setUploadImageupload: null,
+  clickedShelfBoxKeyNumber: null,
+  booksFromLocalStorageBoolean: false,
+  maxBackgroundShelfWidth: 1359,
+  maxBackgroundShelfHeight: 500,
+  backgroundShelvesWidth: 1359,
+  backgroundShelvesHeight: 500,
+  backgroundShelvesTitle: 'kelet-header',
+  backgroundBasicShelfImage: {},
+  backgroundBookShelfImage: {},
+  shelfCoordsObj: null,
+  imgNewSizeRatio: 6.048,
+  imgSizeRatioBookTitle: 0,
+  shelvesCoordsRatio: 1,
   set() {
     this.display = !this.display;
   },
